bda8d1643c400fe63f3dfbf7c44e3610b4281293
Don Ton <donton@utexas.edu>
2017-03-17 20:23:36 -0500

    .gitignore now working

--- adc.d
+++
@@ -1,6 +0,0 @@
-.\adc.o: ADC.c
-.\adc.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\adc.o: ..\tm4c123gh6pm.h
-.\adc.o: ADC.h
-.\adc.o: ST7735.h
-.\adc.o: SysTick.h
--- switch.d
+++
@@ -1,5 +0,0 @@
-.\switch.o: switch.c
-.\switch.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\switch.o: ..\tm4c123gh6pm.h
-.\switch.o: switch.h
-.\switch.o: Sound.h
